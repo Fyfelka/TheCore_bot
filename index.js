d1d666d43385bf48d4acd159d76b433a0bee3bf9
Fyfelka <hedgefog.98@gmail.com>
2019-04-16 18:07:10 +0700

    v0.0.2

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ client.on('message', msg => {
     msg.reply('Pong!')
   }
   if (msg.content === '!faq') {
-	  msg.reply('faq')
+	  msg.reply('FAQ located here - https://docs.google.com/document/d/1QM1Acz96rtFeCz_UC_-gShbxA9Nrcpl-l1nJsulAz1w/. Read it, before asking questions.')
+  }
+  if (msg.content === '!sheet') {
+	  msg.reply('TheCore Spreadsheet - https://docs.google.com/spreadsheets/d/1zN7ufgH79t6uaCXorX6cs3mWfkMKFC_6VTtZ7vH_9-s/. Enjoy!')
+  }
+  if (msg.content === '!thecourse') {
+	  msg.reply('Learn TheCore efficiently - https://docs.google.com/spreadsheets/d/1LhbxeYdkukOzYw030qNYQgLI1p3WUAbSTiuy1emeJSc/.')
   }
 })
 
