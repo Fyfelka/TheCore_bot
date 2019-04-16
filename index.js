c1220936f2118f01d5c814ba5a3feef95172daa9
Fyfelka <hedgefog.98@gmail.com>
2019-04-16 18:32:55 +0700

    v0.0.3

--- index.js
+++ index.js
@@ -10,10 +10,17 @@ client.on('message', msg => {
 	  msg.reply('FAQ located here - https://docs.google.com/document/d/1QM1Acz96rtFeCz_UC_-gShbxA9Nrcpl-l1nJsulAz1w/. Read it, before asking questions.')
   }
   if (msg.content === '!sheet') {
-	  msg.reply('TheCore Spreadsheet - https://docs.google.com/spreadsheets/d/1zN7ufgH79t6uaCXorX6cs3mWfkMKFC_6VTtZ7vH_9-s/. Enjoy!')
+	  msg.reply('TheCore Spreadsheet - https://docs.google.com/spreadsheets/d/1zN7ufgH79t6uaCXorX6cs3mWfkMKFC_6VTtZ7vH_9-s/. And remember - multiple tabs on the bottom. Enjoy!')
   }
   if (msg.content === '!thecourse') {
 	  msg.reply('Learn TheCore efficiently - https://docs.google.com/spreadsheets/d/1LhbxeYdkukOzYw030qNYQgLI1p3WUAbSTiuy1emeJSc/.')
+  }
+  if (msg.content === '!thecourse') {
+	  msg.reply('Insight on the TheCore ideas - https://docs.google.com/document/d/1reGIGIJ4W_MtfyoWjc2TJUeexswA4Jemy23jx0CSsKA/')
+  }
+  if (msg.content === '!tilt') {
+	  const locFile = new Discord.Attachment('./tilt.jpg')
+	  msg.channel.send(locFile)
   }
 })
 
