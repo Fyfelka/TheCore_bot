fef6ddda457ff670de2aa374307e6f3f304b4d1c
Fyfelka <hedgefog.98@gmail.com>
2019-04-16 21:24:17 +0700

    v0.0.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('guildMemberAdd', member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+	const channel = member.guild.channels.find(ch => ch.name === 'start');
 	channel.send('Welcome to TheCore discord server! You can find all documentation and FAQ here - https://drive.google.com/drive/folders/1ui2HNwaUa4FkHzRwATgXHNVEpolLNOzA. Do enjoy your stay, ${member}')
 })
 
