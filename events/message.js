ad0203aa84d3288dbfc85572ed6a1d6fdb91d9a6
Fyfelka <hedgefog.98@gmail.com>
2019-04-17 00:58:32 +0700

    v0.1.0

--- events/message.js
+++ events/message.js
@@ -29,7 +29,7 @@ module.exports = (client, msg) => {
 		msg.reply('Looking how to setup TheCore+? Look here - https://docs.google.com/document/d/1aKCnVl90qck6HbSLQD3jw11bleuVBqtLpYSUb4rsvtI/.')
 	}
 	if (msg.content === '!versions') {
-		msg.reply('TheCoreLite, TheCore, TheCorePlus. \n Easy, Medium, Hard. \n Normal, Less Normal, Least Normal. \n Effecient, More Efficient, Most Efficient.')
+		msg.reply('\n TheCoreLite, TheCore, TheCorePlus. \n Easy, Medium, Hard. \n Normal, Less Normal, Least Normal. \n Effecient, More Efficient, Most Efficient.')
 	}
 	if (msg.content === '!maps') {
 		msg.reply('List of all needed maps, with server-specific links availible in the pinned messages in #map-lab')
